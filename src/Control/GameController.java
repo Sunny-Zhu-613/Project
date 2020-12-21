94938622b276120e57c8b1c2acbe273846f4d70f
Sunny-Zhu-613 <zxrsunny613@gmail.com>
2020-12-21 17:31:09 +0800

    Update Main

---
+++ src/Control/GameController.java
@@ -0,0 +1,70 @@
+package Control;
+
+import Model.Color;
+import Model.Map;
+import View.GameView;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+public class GameController {
+    private Map map;
+    private Color currentTurn;
+    private int dieNumber1, dieNumber2;
+    private int count = 0;
+
+    public GameController() {
+        this.map = new Map();
+        this.currentTurn = Color.BLUE;
+    }
+
+    public Map getMap() {return this.map;}
+    public Color getCurrentTurn() {return this.currentTurn;}
+    public int getDieNumber1() {return this.dieNumber1;}
+    public int getDieNumber2() {return this.dieNumber2;}
+
+    public void rollBtnPressed() {
+        this.dieNumber1 = ThreadLocalRandom.current().nextInt(1, 6 + 1);
+        this.dieNumber2 = ThreadLocalRandom.current().nextInt(1, 6 + 1);
+        System.out.println("(" + dieNumber1 + ", " + dieNumber2 + ")");
+        int sum = dieNumber1 + dieNumber2;
+        if (sum >= 10){
+            count += 1;
+        }
+        if (count >=3){
+            this.currentTurn = currentTurn.next();
+            count = 0;
+        }
+        return;
+    }
+
+    public void addBtnPressed() {
+        int addNumber = Math.min(dieNumber1 + dieNumber2,12);
+        System.out.println(addNumber);
+
+        return;
+    }
+    public void minusBtnPressed() {
+        int minusNumber = Math.max(dieNumber1, dieNumber2) - Math.min(dieNumber1, dieNumber2);
+        System.out.println(minusNumber);
+
+        return;
+    }
+    public void timesBtnPressed() {
+        int timesNumber = Math.min(dieNumber1 * dieNumber2,12);
+        System.out.println(timesNumber);
+
+        return;
+    }
+
+    public void divideBtnPressed() {
+        int divideNumber = Math.max(dieNumber1, dieNumber2) / Math.min(dieNumber1, dieNumber2);
+        System.out.println(divideNumber);
+
+        return;
+    }
+}
