6b7a132e89cf0a5b678caf8f769920c2dd25d0cb
MstMoonshine <38682200@qq.com>
2020-12-21 11:29:20 +0800

    next method implmented

--- src/Model/Color.java
+++ src/Model/Color.java
@@ -3,4 +3,10 @@ package Model;
 public enum Color {
     YELLOW, BLUE, GREEN, RED, BLANK;
     // BLANK for FinalPath
+    public Color next()
+    {
+        return values()[(this.ordinal()+1) % values().length];
+    }
 }
+
+
